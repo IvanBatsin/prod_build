f58ce52143bdee363dfa6c22f048fc5beeea4bdd
ivan_batsin <ltd1584@gmail.com>
2023-05-06 11:13:22 +0300

    fix project structure

--- src/components/Counter.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from './counter.module.scss';
-
-export const Counter: React.FC = () => {
-  return (
-    <div className={styles.counter}>this is counter</div>
-  )
-}

3903edc9909b60ed74c9e09d071cd0b60b05adf6
ivan_batsin <ltd1584@gmail.com>
2023-05-07 12:30:56 +0300

    fix

---
+++ src/widgets/LanguageSwitcher/ui/LanguageSwitcher.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './LanguageSwitcher.module.scss';
+import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Button } from "shared/ui/Button/Button";
+import { CommonComponentProps } from "shared/types/commonTypes";
+
+export const LanguageSwitcher: React.FC<CommonComponentProps> = ({additionalClass}) => {
+  const { t, i18n } = useTranslation();
+
+  const handleToggleLanguage = (): void => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+  }
+  
+  return (
+    <Button 
+      additionalClass={classNames(styles.language_switcher, {}, [additionalClass])} 
+      onClick={handleToggleLanguage}>
+        {t('language')}
+    </Button>
+  )
+}
